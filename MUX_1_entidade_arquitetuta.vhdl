910e2b048507973b81ca1b1fc1a00d0d6c111051
felipeboss232 <felipeboss232@gmail.com>
2019-09-22 20:33:47 -0300

    DONE

--- MUX_1_entidade_arquitetuta.vhdl
+++ MUX_1_entidade_arquitetuta.vhdl
@@ -1,50 +1,24 @@
---Usamos o editor Scriptum (20.0)
---daqui parece tudo bonito
-
 library IEEE;
 use IEEE.std_logic_1164.all;
 
 --Declara todas as entradas e saidas do circuto
 entity mux_1 is
 	port(
-    	in1, in1, sel: in std_logic;
+    	in1, in2, sel: in std_logic;
         s: out std_logic
     );
 end mux_1;
 
 architecture arch of mux_1 is
---PRIMEIRA AND
-	component and1
-		port(x, y : in std_logic;
-			 z : out std_logic
-        );
-        end component;
-        
---SEGUNDA AND
---não sei se daria pra usar a and1 e negar no portmap
---provavelmente daria, fica ai a ideia
-    component and2
-		port((not x), y : in std_logic;
-			 z : out std_logic
-        );
-        end component;
-        
---Declara a OR
-    component or1
-    	port(x, y: in std_logic;
-        	 z : out std_logic
-		);
-	end component;
 
 
 --Declara as linhas entre as and e a or
 signal lin1, lin2 : std_logic;
 
 	begin
-    
---Port map mostrando onde tudo conecta com tudo
-	func_a : and1 port map(in1, sel, lin1);
-    func_b : and2 port map(sel, in1, lin2);
-    func_c : or1  port map(lin1, lin2, s);
-    
+		
+        lin1 <= in2 or sel;
+        lin2 <= (not sel) or in1;
+        
+        s <= lin1 and lin2;
 end arch;
